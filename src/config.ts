daeb126835757ab764a8c82e09004daff6e06202
Kees de Bruin <halfje.bruin@gmail.com>
2024-08-10 15:38:49 +0200

    More updates and stripped some unused stuff

--- src/config.ts
+++ src/config.ts
@@ -13,13 +13,9 @@ export const NAV_ITEMS: NavItems = {
 		path: "/tags",
 		title: "tags",
 	},
-	media: {
-		path: "/media",
-		title: "media",
-	},
 	about: {
-		path: "/about",
-		title: "about",
+		path: "/over-mij",
+		title: "over mij",
 	},
 };
 
@@ -32,24 +28,8 @@ export const SITE = {
 	githubUrl: "https://github.com/kdbruin/halfje-bruin-astro",
 	image:
 		"https://raw.githubusercontent.com/kdbruin/halfje-bruin-astro/main/public/astro-banner.png",
-	// YT video channel Id (used in media.astro)
-	ytChannelId: "",
-	// Optional, user/author settings (example)
-	// Author: name
-	author: "", // Example: Fred K. Schott
-	// Author: Twitter handler
-	authorTwitter: "", // Example: FredKSchott
-	// Author: Image external source
-	authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
-	// Author: Bio
-	authorBio:
-		"Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
 };
 
 // Theme configuration
 export const PAGE_SIZE = 8;
 export const USE_POST_IMG_OVERLAY = false;
-export const USE_MEDIA_THUMBNAIL = true;
-
-export const USE_AUTHOR_CARD = true;
-export const USE_SUBSCRIPTION = false; /* works only when USE_AUTHOR_CARD is true */
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,33 @@
-import path from "path";
+import type { CollectionEntry } from 'astro:content'
+import path from 'path'
+
 const MONTHS = [
-	"Jan",
-	"Feb",
-	"Mar",
-	"Apr",
-	"Mei",
-	"Jun",
-	"Jul",
-	"Aug",
-	"Sep",
-	"Okt",
-	"Nov",
-	"Dec",
-];
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'Mei',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Dec',
+]
 
 export const toTitleCase = (str: string) =>
-	str.replace(/\w\S*/g, function (txt) {
-		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-	});
+    str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    })
 
-export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()];
+export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()]
 
 export const getSlugFromPathname = (pathname: string) =>
-	path.basename(pathname, path.extname(pathname));
+    path.basename(pathname, path.extname(pathname))
+
+export const sortPostsAscending = (posts: CollectionEntry<'blog'>[]) =>
+    posts.sort((a, b) => a.data.date - b.data.date)
+
+export const sortPostsDescending = (posts: CollectionEntry<'blog'>[]) =>
+    posts.sort((a, b) => b.data.date - a.data.date)
