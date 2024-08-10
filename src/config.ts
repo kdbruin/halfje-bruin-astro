7bec98ddca2e9f984d681ccb95ae85d34a8a98a7
Kees de Bruin <halfje.bruin@gmail.com>
2024-08-10 17:29:40 +0200

    Added component for Komoot

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,40 @@
-import type { NavItems } from "./types";
+import type { NavItems } from './types'
 
 export const NAV_ITEMS: NavItems = {
-	home: {
-		path: "/",
-		title: "home",
-	},
-	blog: {
-		path: "/blog",
-		title: "blog",
-	},
-	series: {
-		path: "/series",
-		title: "series",
-	},
-	tags: {
-		path: "/tags",
-		title: "tags",
-	},
-	about: {
-		path: "/over-mij",
-		title: "over mij",
-	},
-};
+    home: {
+        path: '/',
+        title: 'home',
+    },
+    blog: {
+        path: '/blog',
+        title: 'blog',
+    },
+    series: {
+        path: '/series',
+        title: 'series',
+    },
+    tags: {
+        path: '/tags',
+        title: 'tags',
+    },
+    about: {
+        path: '/over-mij',
+        title: 'over mij',
+    },
+}
 
 export const SITE = {
-	// Your site's detail?
-	name: "Halfje-Bruin",
-	title: "Halfje-Bruin.nl",
-	description: "Persoonlijke website van Kees de Bruin",
-	url: "https://halfje-bruin.nl",
-	githubUrl: "https://github.com/kdbruin/halfje-bruin-astro",
-	image:
-		"https://raw.githubusercontent.com/kdbruin/halfje-bruin-astro/main/public/astro-banner.png",
-};
+    // Your site's detail?
+    name: 'Halfje-Bruin',
+    title: 'Halfje-Bruin.nl',
+    description: 'Persoonlijke website van Kees de Bruin',
+    url: 'https://halfje-bruin.nl',
+    githubUrl: 'https://github.com/kdbruin/halfje-bruin-astro',
+    image: 'https://raw.githubusercontent.com/kdbruin/halfje-bruin-astro/main/public/astro-banner.png',
+}
 
 // Theme configuration
-export const PAGE_SIZE = 8;
-export const USE_POST_IMG_OVERLAY = false;
+export const PAGE_SIZE = 8
+export const USE_POST_IMG_OVERLAY = false
+
+export const KOMOOT_SITE_URL = 'https://www.komoot.com/nl-nl'
